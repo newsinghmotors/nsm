7c4e64aa12c9223c65245b25f64b314d0c61fcc8
newsinghmotors <138613589+newsinghmotors@users.noreply.github.com>
2026-02-09 13:58:15 +0530

    email template updated

--- feed/forms.py
+++ feed/forms.py
@@ -7,6 +7,7 @@ class NewKeyForm(forms.ModelForm):
 		fields = '__all__'
 
 class EmailForm(forms.Form):
-    full_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Recipient Name'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Recipient Email'}))
-    otp_code = forms.CharField(max_length=6, widget=forms.TextInput(attrs={'placeholder': 'Enter OTP'}))
+    full_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Customer Name'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Customer Email'}))
+    vehicle_info = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Vehicle Details'}))
+    key_code = forms.CharField(max_length=6, widget=forms.TextInput(attrs={'placeholder': 'Enter Key Code'}))
